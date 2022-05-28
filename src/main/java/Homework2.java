f195db9178b5c9d237283a4e9cbb9b853350b0c5
Tamara <tamaragluhovic@gmail.com>
2022-05-28 19:46:33 +0200

    Initial commit

---
+++ src/main/java/Homework2.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tamara\\IdeaProjects\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+        driver.navigate().to("https://www.stealmylogin.com/demo.html");
+        driver.findElement(By.name("username")).sendKeys("test");
+
+        WebElement inputPassword = driver.findElement(By.name("password"));
+        inputPassword.sendKeys("123456");
+        inputPassword.sendKeys(Keys.ENTER);
+
+        if (driver.getTitle().equals("Example Domain")){
+            System.out.println("Nice");
+        }else {
+            System.out.println("Not nice");
+        }
+
+
+    }
+}
